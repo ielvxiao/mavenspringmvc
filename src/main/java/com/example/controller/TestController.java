8ed34a3d016f2a03b19fb1ea510ed079cc02be23
Clark <ielvxiao@gmail.com>
2018-07-22 18:12:19 +0800

    spring mvc hello world.

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -2,15 +2,14 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/mvc")
+@RequestMapping("")
 public class TestController {
     
-    @RequestMapping(value = "/hello")
-    @ResponseBody
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String hello() {
-        return "hello";
+        return "index";
     }
 }

1f7d8a1709236bfe8b2890199df8c33c05778a66
Clark <ielvxiao@gmail.com>
2018-07-22 14:54:24 +0800

    git init

---
+++ src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/mvc")
+public class TestController {
+    
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}

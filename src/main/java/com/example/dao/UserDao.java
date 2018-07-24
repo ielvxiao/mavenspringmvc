8a71518a4ceff5a1e5e7e34b9a860e017b8090c0
lvxiao <lvxiao@jd.com>
2018-07-24 19:55:55 +0800

    修改代码，目前暂不支持json格式返回。有待优化

--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -1,10 +1,12 @@
 package com.example.dao;
 
 import com.example.domain.User;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by lvxiao on 2018/7/24.
  */
+@Repository
 public interface UserDao {
     User selectUserById(Integer id);
 }

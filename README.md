fe91fa73c78a57dc17b1636e80010ec6f620c4b5
lvxiao <lvxiao@jd.com>
2018-08-29 18:31:58 +0800

    修改一些配置方式,还未验证

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 一些针对sprig mvc的练习
-test resrt
+
+配置方式参考了：https://github.com/KevinYounGa/spring4-mybatis3-cxf3-maven/blob/master/spring_mvc_cxf-server/src/main/resources/spring/spring-mvc.xml
--- src/main/test/WebserviceTest.java
+++ src/main/test/WebserviceTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by lvxiao on 2018/8/26.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/applicationContext.xml","/dispatcher-servlet.xml"})
+@ContextConfiguration(locations = {"/dispatcher-servlet.xml","/spring-cxf.xml"})
 public class WebserviceTest {
 
     private TestWebService testWebService;

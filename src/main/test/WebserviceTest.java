710598fd79b0d6dd3c7f637f8ff7d78424c75014
lvxiao <lvxiao@jd.com>
2018-08-31 16:31:26 +0800

    修改部分配置

--- src/main/test/WebserviceTest.java
+++ src/main/test/WebserviceTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by lvxiao on 2018/8/26.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/dispatcher-servlet.xml","/spring-cxf.xml"})
+@ContextConfiguration(locations = {"/applicationContext.xml"})
 public class WebserviceTest {
 
     private TestWebService testWebService;

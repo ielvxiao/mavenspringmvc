03ee25d9dbae96a8c6593409bd19369d5c124cd8
lvxiao <lvxiao@jd.com>
2018-09-07 18:51:30 +0800

    代码优化，增加mockito框架

--- src/main/test/com/exampale/service/DepartmentsTest.java
+++ src/main/test/com/exampale/service/DepartmentsTest.java
@@ -6,6 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by lvxiao on 2018/9/7.
@@ -14,11 +18,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {"/applicationContext.xml", "/dispatcher-servlet.xml"})
 public class DepartmentsTest {
 
+    private MockMvc mockMvc;
+
     @Autowired
     private DepartmentsService departmentsService;
 
     @Test
     public void selectDepartmentById() {
+        try {
+            mockMvc.perform(get("/department/select")
+            .param("id", "d001"))
+            .andExpect(status().is(200))
+            .andExpect(content().string("success"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println(departmentsService.selectDepartmentById("d001"));
     }
 }

ec456c6157064e90537e41975dd725b2e2d74da6
lvxiao <lvxiao@jd.com>
2018-09-25 14:50:08 +0800

    代码优化

--- src/main/java/com/example/aop/SpringTest.java
+++ src/main/java/com/example/aop/SpringTest.java
@@ -1,23 +1,14 @@
 package com.example.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
-
 /**
  * Created by lvxiao on 2018/9/18.
  */
-@Aspect
-@Component
 public class SpringTest {
-    @Pointcut(value = "execution(* *..UserService*.*(..))")
     public void doBefore() {
-
+        System.out.println("方法执行前执行了");
     }
 
-    @Before("doBefore()")
-    public void test() {
-        System.out.println("方法执行前执行了");
+    public void doAfter() {
+        System.out.println("方法执行后执行了");
     }
 }

d25df1bd328157ccb5a4866c8383eefe8e80e8c3
lvxiao <lvxiao@jd.com>
2018-11-02 17:30:47 +0800

    代码优化

--- src/main/java/com/example/aop/DataSourceInterceptor.java
+++ src/main/java/com/example/aop/DataSourceInterceptor.java
@@ -9,9 +9,11 @@ import com.example.util.DataSourceTypeManager;
 public class DataSourceInterceptor {
     public void setDataSourceMaster() {
         DataSourceTypeManager.set(DataSources.MASTER);
+        System.out.println("数据源变换为MASTER");
     }
 
     public void setDataSourceSLAVE() {
         DataSourceTypeManager.set(DataSources.SLAVE);
+        System.out.println("数据源变换为SLAVE");
     }
 }
